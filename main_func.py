d643f0a3b5dfd16f612e0d171300c9fe4ff7e66e
A1ex Mac <frolov1984@gmail.com>
2022-09-14 23:01:46 -0400

    speed improvement

--- main_func.py
+++ main_func.py
@@ -11,7 +11,7 @@
 # * (asterisk)
 
 import time
-from typing import List
+from typing import List, Tuple
 from PIL import Image
 import cv2
 import imutils
@@ -24,6 +24,8 @@ import os
 import csv
 import shutil  # to remove folder recursively
 
+from threading import Thread
+from queue import Queue
 
 # Timer of the script start
 start_time = time.monotonic()
@@ -33,9 +35,9 @@ root_path = argv[1]
 
 
 # Extract file name and file path   // not in use
-def path_leaf(any_path: str) -> str:
+def path_leaf(any_path: str) -> tuple[str, str]:
     head, tail = ntpath.split(any_path)
-    return tail
+    return head, tail
 
 
 def split_file_name(filename):
@@ -50,20 +52,27 @@ video_source_path = 'VideoSourceFiles/'
 img_root_path = root_path + '/jpg/'
 
 
+def get_absolut_file_path(file):
+    absolute_file_path = os.path.abspath(file)
+    return absolute_file_path
+
+
 def make_dir(dir_path: str) -> str:
-    print('DEBUG', dir_path)
+    # print('DEBUG', dir_path)
     extracted_dir_name = path_leaf(dir_path)
-    print('DEBUG', extracted_dir_name)
+    # print('DEBUG', extracted_dir_name)
     if not os.path.exists(dir_path):
         try:
             os.mkdir(dir_path)
         except OSError:
             print(f'can\'t create {extracted_dir_name} directory, path: {dir_path}')
-            pass
+    return dir_path
 
 
 make_dir(img_root_path)  # create JPG directory
 
+
+# print(f'Make_dir *Function {time.monotonic() - start_time}')
 
 # Returns tuple of directories (to save RAM)
 # def dirs_by_path_as_tup(dir_path: str) -> tuple:
@@ -91,125 +100,136 @@ def files_list_by_path_filter_by_ext(file_path: str, ext: str) -> list:
     return sorted(list_of_files_by_ext)
 
 
+mp4_file_list = files_list_by_path_filter_by_ext(root_path, '.mp4')
+
+
+# print(f'Get file list *Function {time.monotonic() - start_time}')
+
 # DEBUG print('VIDEO_FILES :', files_list_by_path_filter_by_ext(video_source_path, '.mp4'))
 # DEBUG print('IMAGE_FILES :', files_list_by_path_filter_by_ext(img_root_path, '.jpg'))
+def create_jpg_folder(folder_path):
+    os.mkdir(folder_path)
+    return folder_path
 
 
 # Extract frames as JPG and distribute them in folders named as Video files
-def extract_frames_and_distribute(video_files_list):
+def extract_frames(video_files_list: list) -> list:
     jpg_folders_list = []
-    for video_file in video_files_list:
-        f_name, f_ext = split_file_name(video_file)
-        # split_tup = os.path.splitext(video_file)  # split file name and extension
-        # DEBUG print(split_tup[0])  # take file name, for ext use [1]
-        # f_name = split_tup[0]  # use file name for category naming
-        jpg_folders_list.append(f_name)
-        folder_path = f'{root_path}/jpg/{f_name}'
-        if not os.path.exists(folder_path):
-            make_dir(folder_path)
-            cap = cv2.VideoCapture(
-                # TEST1 video_source_path + video_file)  # cap = cv2.VideoCapture(os.path.abspath(video_file))
-                root_path + '/' + video_file)  # cap = cv2.VideoCapture(os.path.abspath(video_file))
 
-            # Read video parameters
-            # frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
+    for mp4_file in video_files_list:
+        f_name, f_ext = split_file_name(mp4_file)
+        # f_path = path_leaf(mp4_file_path)  # VideoSourceFiles/, //Users../SearchInsideVideoFiles/VideoSourceFiles/jpg/
+        mp4_file_path = root_path + '/' + mp4_file  # full file path for Capturing
+        folder_path = f'{img_root_path}{f_name}'  # TO BE DELETED TEMP!!!!!!!!!!!
+        print('folder_path',folder_path)
+        if not os.path.exists(folder_path):  # TO BE DELETED TEMP!!!!!!!!!!!
+            jpg_folder = create_jpg_folder(folder_path)  # function create a folder for JPGs
+            cap = cv2.VideoCapture(mp4_file_path)
             total_frames_per_file = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-            # checks whether frames were extracted
-            # while cap.isOpened():
-            success, image = cap.read()
+            success, jpg = cap.read()
             count = 0
             while success:
-                jpg_file_path = f'{folder_path}/{total_frames_per_file}frames_{str(count).zfill(6)}.jpg'
-                cv2.imwrite(jpg_file_path, image)
-                success, image = cap.read()
-                # DEBUG print('Read a new frame: ', success)
+                jpg = imutils.resize(jpg, width=480)
+                jpg = cv2.cvtColor(jpg, cv2.COLOR_BGR2GRAY)[90:180, 0:480]
+                jpg = np.dstack([jpg, jpg, jpg])
+                jpg_file_path = f'{jpg_folder}/{f_name}_{str(count).zfill(6)}.jpg'
+                cv2.imwrite(jpg_file_path, jpg)
+                success, jpg = cap.read()
+                print('Read a new frame: ', success)
                 count += 1
-
-                # Read image and define high and width
-                img = cv2.imread(jpg_file_path)
-                h, w = int(img.shape[0]) // 3, int(img.shape[1] // 3)   # 1/3 of original size
-                resized_size = (w, h)
-                resized_img = cv2.resize(img, resized_size, interpolation=cv2.INTER_CUBIC)
-                cv2.imwrite(jpg_file_path,
-                            cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)[int(h * 0.333):int(h * 0.666),
-                            0:w])  # Write & convert to grayscale, extract middle part of the image. Speed up...
-
-            # break
-            # cv2.destroyAllWindows()
-            # cap.release()
+                jpg_folders_list.append(jpg)
+                # print('JPGs', jpg_folders_list)
         else:
-            try:
-                # Delete folder recursively
-                shutil.rmtree(folder_path)
-            except:
-                print(f'Error. Can\'t delete dolder {folder_path}')
-
+            try:  # TO BE DELETED TEMP!!!!!!!!!!!
+                # Delete folder recursively TO BE DELETED TEMP!!!!!!!!!!!
+                shutil.rmtree(folder_path)  # TO BE DELETED TEMP!!!!!!!!!!!
+            except:  # TO BE DELETED TEMP!!!!!!!!!!!
+                print(f'Error. Can\'t delete folder {folder_path}')  # TO BE DELETED TEMP!!!!!!!!!!!
     return jpg_folders_list
 
 
-# Save hashes per video
-def build_hashes_list_file(img_folders_list: str) -> str:  # write hashes into CSV file
-    for folder in img_folders_list:
-        for (root, dirs, files) in os.walk(img_root_path + folder):
-            header = ['VideoFileName', 'Hashes']   # building csv header
-            with open(root + '/' + folder + '.csv', 'a', encoding='UTF8') as hashes_per_video_file:
-                writer = csv.writer(hashes_per_video_file)
-                # write the header
-                writer.writerow(header)
-                for jpg_file in files:
-                    img_object = cv2.imread(img_root_path + folder + '/' + jpg_file)
-                    image_pil = Image.fromarray(img_object)
-                    hshperceptual = hsh.phash(image_pil, 10)  # number of hash characters, need to check
-
-                    csv_data_row = [folder, str(hshperceptual)]  # building csv row
-                    writer.writerow(csv_data_row)  # write row
-
-    return img_folders_list
+extract_frames(mp4_file_list)
 
 
-jpg_folder = extract_frames_and_distribute(files_list_by_path_filter_by_ext(root_path, '.mp4'))  # list of all JPG
-# folders
-build_hashes_list_file(jpg_folder)  # write hashes in file
-
-csv_hash_files: list[str] = []  # prepare a placeholder for the function: build_hashes_list
+def distribute():
+    pass
+    return jpg_folders_list
 
 
-# Build list of CSV files, which contain hashes to go through them
-def build_hashes_list(img_path: str) -> list:
-    for root, dirs, files in os.walk(img_path):
-        for file in files:
-            if file.endswith(".csv"):
-                # print('DEBUG: ', os.path.join(root, file))
-                csv_hash_files.append(os.path.join(root, file))  # build path to TXT in order to read it
-    return csv_hash_files
+#
+#
+# jpg_folders = extract_frames_and_distribute(mp4_file_list)  # list of all JPG folders
+# print(f'Extract frames *Function {time.monotonic() - start_time}')
+#
+#
+# # Save hashes per video
+# def build_hashes_list_file(img_folders_list: str) -> str:  # write hashes into CSV file
+#     for folder in img_folders_list:
+#         for (root, dirs, files) in os.walk(img_root_path + folder):
+#             header = ['VideoFileName', 'Hashes']   # building csv header
+#             with open(root + '/' + folder + '.csv', 'a', encoding='UTF8') as hashes_per_video_file:
+#                 writer = csv.writer(hashes_per_video_file)
+#                 # write the header
+#                 writer.writerow(header)
+#                 for jpg_file in files:
+#                     img_object = cv2.imread(img_root_path + folder + '/' + jpg_file)
+#                     image_pil = Image.fromarray(img_object)
+#                     hshperceptual = hsh.phash(image_pil, 10)  # number of hash characters, need to check
+#
+#                     csv_data_row = [folder, str(hshperceptual)]  # building csv row
+#                     writer.writerow(csv_data_row)  # write row
+#
+#     return img_folders_list
+#
+#
+#
+# build_hashes_list_file(jpg_folders)  # write hashes in file
+#
+# csv_hash_files: list[str] = []  # prepare a placeholder for the function: build_hashes_list
+#
+#
+# # Build list of CSV files, which contain hashes to go through them
+# def build_list_of_csvs(img_path: str) -> list:
+#     for root, dirs, files in os.walk(img_path):
+#         for file in files:
+#             if file.endswith(".csv"):
+#                 # print('DEBUG: ', os.path.join(root, file))
+#                 csv_hash_files.append(os.path.join(root, file))  # build path to TXT in order to read it
+#     return csv_hash_files
+#
+#
+# hashes_list = build_list_of_csvs(img_root_path)
+# print(f'Build list of CSV *Function {time.monotonic() - start_time}')
+# result_file = img_root_path + 'result.csv'
+#
+#
+# # Concatenate all CSV files with hashes
+# def concatenate_hash_files(result_csv_path: str, hash_csv_list: list) -> str:
+#     with open(result_csv_path, 'w') as combined_file:
+#         for csv_file in hash_csv_list:
+#             with open(csv_file) as source_file:
+#                 combined_file.write(source_file.read())
+#
+#
+# concatenate_hash_files(result_file, hashes_list)
+# print(f'Concatenate CSV *Function {time.monotonic() - start_time}')
 
-
-build_hashes_list(img_root_path)
-
-result_file = img_root_path + 'result.csv'
-
-
-# Concatenate all CSV files with hashes
-def concatenate_hash_files(result_csv_path: str, hash_csv_list: list) -> str:
-    with open(result_csv_path, 'w') as combined_file:
-        for csv_file in hash_csv_list:
-            with open(csv_file) as source_file:
-                combined_file.write(source_file.read())
-
-
-concatenate_hash_files(result_file, build_hashes_list(img_root_path))
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
 
 
 # Compare hashes for matches
-def compare_files_by_group(total_num_frames, hash_list):
-    chunks = (total_num_frames - 1) // 5 + 1
-    for i in range(chunks):
-        jpg_batch = hash_list[i * 50:(i + 1) * 50]
-    return jpg_batch
+# def compare_files_by_chunk(num_frames: int, hash_list: list) -> list:
+#     chunks = (num_frames - 1) // 5 + 1
+#     for i in range(chunks):
+#         pass
+#         # jpg_batch = hash_list[i * 50:(i + 1) * 50]
+#     return jpg_batch
 
 
-
+# print(compare_files_by_chunk(!!!!total_number_of_frames!!!!!, concatenate_hash_files))
+# print(f'Compare files by chunk *Function {time.monotonic() - start_time}')
 # im_pil = Image.fromarray(
 #     img)  # Takes the array object as the input and returns the image object made from the array object.
 #
@@ -367,5 +387,4 @@ def compare_files_by_group(total_num_frames, hash_list):
 # if hash_difference == 0:
 #     print(hash_difference)
 
-print(f'SCRIPT TOOK {time.monotonic() - start_time}')
-
+print(f'TOTAL SCRIPT TOOK {time.monotonic() - start_time}')
